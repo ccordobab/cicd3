038a3558136b4ace971c85923ae10eb4faf17157
Camilo Cordoba <ccordobab@eafit.edu.co>
2026-04-19 18:18:04 -0500

    issue corrected

--- app/calculadora.py
+++ app/calculadora.py
@@ -2,7 +2,7 @@
 
 """Funciones básicas de una calculadora."""
 
-AUTOR = "Daniela, Juan Miguel, Camilo y David"  # IMPORTANTE: Reemplaza con tu nombre completo (debe coincidir con el nombre que uses en cualquier otro identificador del proyecto. Sugerencia: Usuario de correo de EAFIT)
+AUTOR = "Daniela, Juan Miguel, Camilo y David" 
 
 
 def sumar(a, b):
